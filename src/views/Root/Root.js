39f1be26010d5ee6813c847b63d300faba918e51
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-08-06 17:49:27 +0200

    Basic folders and views were added. Unnecessary stuff was removed

---
+++ src/views/Root/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Root() {
+    return (
+        <>
+            <h1>This is gonna be a Workers List :)</h1>
+        </>
+    )
+}
+
+
+export default Root;
