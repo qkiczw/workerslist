4f462a8b626fe6e8cffc66c4899963b49bbfc74f
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-08-11 20:10:14 +0200

    I added some code to display worker info, action icons in table and style it a little bit

--- src/components/WorkersList/WorkersList.js
+++ src/components/WorkersList/WorkersList.js
@@ -1,20 +1,37 @@
 import React from 'react';
 
+//Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserMinus, faEdit, faSortAlphaUp } from '@fortawesome/free-solid-svg-icons';
+
 function WorkersList( props ) {
     const [...workers] = props.workers; 
     return (
         <>
             <h2>Tutaj będzie lista pracowników</h2>
-            <ul>
-                {workers.map( worker => 
-                    <li key={worker.id} > 
-                        <div>
-                            <span> {worker.name} </span>
-                            <span> {worker.lastName}</span>
-                        </div>
-                    </li>)
-                }
-            </ul>
+            <table className={'workersListTable'}>
+                <thead>
+                    <tr>
+                        <th>Imię <span><FontAwesomeIcon icon={faSortAlphaUp} /></span></th>
+                        <th>Nazwisko <span><FontAwesomeIcon icon={faSortAlphaUp} /></span></th>
+                        <th>Stanowisko</th>
+                        <th>Działanie</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {workers.map( worker =>  
+                        <tr key={worker.id} >
+                            <td> {worker.name} </td>
+                            <td> {worker.lastName}</td>
+                            <td> {worker.position}</td>
+                            <td>
+                                <span><FontAwesomeIcon icon={faEdit} /></span>
+                                <span><FontAwesomeIcon icon={faUserMinus} /></span>
+                            </td>
+                        </tr>
+                        )}
+                </tbody>    
+            </table>
         </>
     )
 }
