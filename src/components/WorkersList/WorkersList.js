1a071f6c59a45c91797c41293efb44e54f89e208
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-08-13 17:12:19 +0200

    I just added rebuild table and i added to it some styles

--- src/components/WorkersList/WorkersList.js
+++ src/components/WorkersList/WorkersList.js
@@ -2,37 +2,36 @@ import React from 'react';
 
 //Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserMinus, faEdit, faSortAlphaUp } from '@fortawesome/free-solid-svg-icons';
+import { faUserMinus, faEdit, faSortAlphaUp, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 function WorkersList( props ) {
     const [...workers] = props.workers; 
     return (
-        <>
-            <h2>Tutaj będzie lista pracowników</h2>
+        
             <table className={'workersListTable'}>
-                <thead>
+                <thead className={'workersListTable__titles'}>
                     <tr>
-                        <th>Imię <span><FontAwesomeIcon icon={faSortAlphaUp} /></span></th>
-                        <th>Nazwisko <span><FontAwesomeIcon icon={faSortAlphaUp} /></span></th>
-                        <th>Stanowisko</th>
-                        <th>Działanie</th>
+                        <th className={'workersListTable__cell workersListTable__cell--title workersListTable__col--name'}>Imię <span><FontAwesomeIcon icon={faSortAlphaUp} /></span></th>
+                        <th className={'workersListTable__cell workersListTable__cell--title workersListTable__col--last-name'}>Nazwisko <span><FontAwesomeIcon icon={faSortAlphaUp} /></span></th>
+                        <th className={'workersListTable__cell workersListTable__cell--title workersListTable__col--position'}>Stanowisko</th>
+                        <th className={'workersListTable__cell workersListTable__cell--title workersListTable__col--action'}>Działanie</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className={'workersListTable__content'}>
                     {workers.map( worker =>  
-                        <tr key={worker.id} >
-                            <td> {worker.name} </td>
-                            <td> {worker.lastName}</td>
-                            <td> {worker.position}</td>
-                            <td>
-                                <span><FontAwesomeIcon icon={faEdit} /></span>
-                                <span><FontAwesomeIcon icon={faUserMinus} /></span>
+                        <tr key={worker.id} className={'workersListTable__row'}>
+                            <td className={'workersListTable__cell workersListTable__col--name'}> {worker.name} </td>
+                            <td className={'workersListTable__cell workersListTable__col--last-name'}> {worker.lastName}</td>
+                            <td className={'workersListTable__cell workersListTable__col--position'}> {worker.position}</td>
+                            <td className={'workersListTable__cell workersListTable__col--action'}>
+                                <FontAwesomeIcon icon={faInfoCircle } className={'workersListTable__cell__action-icon'} />
+                                <FontAwesomeIcon icon={faEdit} className={'workersListTable__cell__action-icon'}/>
+                                <FontAwesomeIcon icon={faUserMinus} className={'workersListTable__cell__action-icon'}/>
                             </td>
                         </tr>
                         )}
                 </tbody>    
             </table>
-        </>
     )
 }
 
