1576de691e11c7cbafcfd20505259e7e65923ce7
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-08-18 17:14:27 +0200

    Simple temporary data base was added. Workers list component was created

---
+++ src/data/dataBase.js
@@ -0,0 +1,66 @@
+const dataBase = {
+    workers : [
+        {
+            id: 1,
+            name: 'Wojciech',
+            lastName: 'Kowalski',
+            position: 'Kierownik',
+        },
+        {
+            id: 2,
+            name: 'Rafał',
+            lastName: 'Nizielski',
+            position: 'Pracownik',
+        },
+        {
+            id: 3,
+            name: 'Łukasz',
+            lastName: 'Tomalski',
+            position: 'Pracownik',
+        },
+        {
+            id: 4,
+            name: 'Daniel',
+            lastName: 'Zagralski',
+            position: 'Pracownik',
+        },
+        {
+            id: 5,
+            name: 'Janusz',
+            lastName: 'Lipski',
+            position: 'Pracownik',
+        },
+        {
+            id: 6,
+            name: 'Bartosz',
+            lastName: 'Smużewski',
+            position: 'Pracownik',
+        },
+        {
+            id: 7,
+            name: 'Teodor',
+            lastName: 'Villan',
+            position: 'Pracownik',
+        },
+        {
+            id: 8,
+            name: 'Maciej',
+            lastName: 'Wilk',
+            position: 'Pracownik',
+        },
+        {
+            id: 9,
+            name: 'Roman',
+            lastName: 'Basar',
+            position: 'Pracownik',
+        },
+        {
+            id: 10,
+            name: 'Grand',
+            lastName: 'Torino',
+            position: 'Pracownik',
+        },
+    ]
+}
+
+export default dataBase;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // Components
 import TopBar from '../../components/Topbar/Topbar';
@@ -6,16 +6,19 @@ import WorkersList from '../../components/WorkersList/WorkersList';
 
 // Data
 import workersArray from '../../data/workersArray';
+import dataBase from '../../data/dataBase';
 
 function Root() {
+    
     const workersTotalAmount = workersArray.length;
+
     return (
         <>
            <div className="appContainer">
                 <TopBar />
                 <p>This is main container</p>
                 <div>W bazie znajduję się {workersTotalAmount} pracowników</div>
-                <WorkersList workers={workersArray}/>
+                <WorkersList workers={dataBase.workers}/>
            </div>
         </>
     )
